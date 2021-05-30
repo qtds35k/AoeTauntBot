29a0bba1810ae700eeccb669bfa9bdd04d886d5e
tpysz5n <o12116ex@gmail.com>
2021-05-29 19:21:28 -0700

    add auto-disconnect logic

--- bot.py
+++ bot.py
@@ -31,6 +31,21 @@ async def taunt(ctx):
     source = FFmpegPCMAudio(tauntUrl)
 
     player = voice.play(source)
-    return
+    
+    while voice.is_playing(): # Checks if voice is playing
+        await asyncio.sleep(1) # While it's playing it sleeps for 1 second
+    else:
+        await asyncio.sleep(15) # If it's not playing it waits 15 seconds
+        while voice.is_playing(): # and checks once again if the bot is not playing
+            break # if it's playing it breaks
+        else:
+            await voice.disconnect() # if not it disconnects
+
+@client.command(name='.0')
+async def leave(ctx):
+    server = ctx.message.guild.voice_client
+    await server.disconnect()
+
+
 
 client.run(TOKEN)
