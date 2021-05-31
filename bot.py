98d6d660270ddb383e9f4b77d87f110b149b51c2
tpysz5n <o12116ex@gmail.com>
2021-05-31 00:12:32 -0700

    revise log message

--- bot.py
+++ bot.py
@@ -47,7 +47,7 @@ async def taunt(ctx):
     try:
         player = voice.play(source)
     except:
-        print("Another taunt is playing. Dropping this request")
+        print("Another taunt is playing. Dropping this request: " + await ctx.message.content)
 
     while voice.is_playing():
         await asyncio.sleep(60)
