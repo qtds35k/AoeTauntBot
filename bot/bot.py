a92c4d0e2a7ca3a1e7af34eb94d0254e39104859
qtds35k <sh.wu@nyu.edu>
2024-04-26 21:45:50 -0700

    add member taunts

--- bot/bot.py
+++ bot/bot.py
@@ -19,7 +19,7 @@ async def on_ready():
     # await client.change_presence(status=discord.Status.idle, activity=discord.Game(name="你媽的奶奶"))
     print('TauntBot onboard.')
 
-@client.command(aliases=['1', '2', '2ja', '3', '4', '5', '6', '7', '8', '9', '11', '12', '13', '14', '18', '23', '24', '29', '30', '34', '35', '41', '69', '104', '105', '301', '302', 'agu', 'ah', 'ahnia', 'baan', 'bling', 'brush', 'call', 'crap', 'dick', 'dick2', 'dick3', 'die', 'die2', 'dio', 'dog', 'door', 'dunno', 'eh', 'eh2', 'fine', 'fine2', 'fine3', 'g', 'gan', 'gan2', 'gan3', 'gg', 'gibai', 'go', 'go2', 'guan', 'hehe', 'hey', 'hey2', 'imp', 'in', 'in2', 'iyo', 'justice', 'lilai', 'luv', 'ma', 'maja', 'majaja', 'me', 'nene', 'no', 'nodick', 'oyo', 'perv', 'pogo', 'quack', 'roger', 'say', 'spag', 'turk', 'up', 'wait', 'wifi', 'yahoo', 'yay', 'yay2', 'zawarudo'])
+@client.command(aliases=['1', '2', '2ja', '3', '4', '5', '6', '7', '8', '9', '11', '12', '13', '14', '18', '23', '24', '29', '30', '34', '35', '41', '69', '104', '105', '301', '302', 'agu', 'ah', 'ah2', 'ahnia', 'baan', 'bb', 'bb2', 'bling', 'brush', 'call', 'crap', 'dick', 'dick2', 'dick3', 'die', 'die2', 'dio', 'dog', 'door', 'dunno', 'eh', 'eh2', 'fine', 'fine2', 'fine3', 'g', 'gan', 'gan2', 'gan3', 'gg', 'gibai', 'go', 'go2', 'guan', 'hehe', 'hey', 'hey2', 'imp', 'in', 'in2', 'iyo', 'justice', 'lilai', 'lager', 'luv', 'ma', 'maja', 'majaja', 'me', 'nene', 'nice', 'no', 'no2', 'nodick', 'oyo', 'perv', 'pogo', 'quack', 'roger', 'say', 'spag', 'turk', 'up', 'wait', 'where', 'wifi', 'yahoo', 'yay', 'yay2', 'zawarudo'])
 async def taunt(ctx):
     botMessage = ''
     if ctx.message.author.voice == None:
@@ -80,7 +80,7 @@ async def help(ctx):
     otherAoeSounds = '301/302 (MBL wololo), agu, bling, brush, crap, dog, door, eh, eh2, fine, gg, hey, hey2, lilai, ma, perv, pogo'
     embed.add_field(name='Other AOE sounds', value=otherAoeSounds, inline=False)
     
-    additionalTaunt = '2ja, 41, 69, ah, ahnia, baan, call, dick, dick2, dick3, die, die2, dio, dunno, fine2, fine3, g, gan, gan2, gan3, gibai, go, go2, guan, hehe, imp, in, in2, iyo, justice, luv, maja, majaja, me, nene, no, nodick, oyo, quack, roger, say, spag, turk, up, wait, wifi, yahoo, yay, yay2, zawarudo'
+    additionalTaunt = '2ja, 41, 69, ah, ah2, ahnia, baan, bb, bb2, call, dick, dick2, dick3, die, die2, dio, dunno, fine2, fine3, g, gan, gan2, gan3, gibai, go, go2, guan, hehe, imp, in, in2, iyo, justice, lager, luv, maja, majaja, me, nene, nice, no, no2, nodick, oyo, quack, roger, say, spag, turk, up, wait, wifi, yahoo, yay, yay2, where, zawarudo'
     embed.add_field(name='Additional taunts', value=additionalTaunt, inline=False)
     
     await channel.send(embed=embed)
